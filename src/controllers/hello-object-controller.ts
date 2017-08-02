477432a2b0c252557a81ba89bb405c5f47f00a9a
Val Rudi <mail.valik@gmail.com>
2017-08-02 18:00:43 +0300

    Added more examples with Promises and Swagger reaction on JSDoc

--- src/controllers/hello-object-controller.ts
+++ src/controllers/hello-object-controller.ts
@@ -4,8 +4,20 @@ import {SimpleHello} from '../models/simple-model';
 /**
  * This is a demo operation to show how to use typescript-rest library.
  */
-@Path('/hello-object')
+@Path('/hello-objects')
 export class HelloObjectController {
+
+    /**
+     * Send a list of objects with greeting message.
+     * @return Array<SimpleHello> simple array of objects
+     */
+    @GET
+    sayArrayObjectHello(): Array<SimpleHello> {
+        return [
+            new SimpleHello('1'),
+            new SimpleHello('2')
+        ];
+    }
 
     /**
      * Send a object with greeting message.
@@ -15,6 +27,22 @@ export class HelloObjectController {
     @GET
     sayObjectHello(@PathParam('name') name: string): SimpleHello {
         return new SimpleHello(name);
+    }
+
+    /**
+     * Send a list of objects with greeting message wrapped in Promise
+     * @param name The name that will receive our greeting message
+     * @return Array<SimpleHello> simple array of objects
+     */
+    @Path(':name/promises')
+    @GET
+    sayArrayObjectPromiseHello(@PathParam('name') name: string): Promise<Array<SimpleHello>> {
+        return new Promise((resolve, reject) => {
+            resolve([
+                new SimpleHello(name + '1'),
+                new SimpleHello(name + '2')
+            ]);
+        });
     }
 
     /**
